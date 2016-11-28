d9a8fc63bac30734e7ea0a8a7147ea114cc7b7ca
vitid <view.cp32@gmail.com>
2016-11-27 18:22:50 -0800

    Update crawler to crawl individual's review score
    * Update crawler
    * Collect review data

--- scrapyIMDB/scrapyIMDB/spiders/reviews_spider.py
+++ scrapyIMDB/scrapyIMDB/spiders/reviews_spider.py
@@ -20,20 +20,31 @@ class ReviewsSpider(scrapy.Spider):
 
     def parse(self, response):
 
-        reviews = response.xpath("//layer/div[4]/div[3]/div[3]/div[3]/p").extract()
+        content = response.xpath("//div[@id='tn15content']")[0]
+
+        headers = content.xpath("div[not(@id)]")
+        reviews = content.xpath("p").extract()
         # this indicates that no more review to crawl, terminate the program
         if(len(reviews) <= 1):
             return
-        # Exclude the last one because it is a link to 'Add another review'
-        for review in reviews[0:len(reviews)-1]:
-            if(len(review) > self.MAX_CHAR_LENGTH):
+
+        for index in range(0,len(headers)):
+            if headers[index].xpath("img/@alt").extract_first() == None:
+                #no score is given for this review
                 continue
 
+            review = reviews[index]
+            if (len(review) > self.MAX_CHAR_LENGTH):
+                continue
+
+            score = headers[index].xpath("img/@alt").extract_first().split("/")[0]
             self.numCollect += 1
-            yield{
-                #strip <p>...</p> tag
-                'review': review[3:-4]
+            yield {
+                # strip <p>...</p> tag
+                'review': review[3:-4],
+                'score': score
             }
+
         self.pageNum += 1
         # stop if the crawling limit is reached
         if (self.MAX_PAGE_NUM!=-1 and self.pageNum > self.MAX_PAGE_NUM):
